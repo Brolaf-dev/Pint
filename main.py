36b71fb64d8c1b2d5a79e88d11e2703b4e9e1410
minhtrikieu <50952916+minhtrikieu@users.noreply.github.com>
2020-06-17 10:28:24 +0200

    Add files via upload
    the button is added, still working on the client and the display

--- main.py
+++ main.py
@@ -1,111 +1,69 @@
-import wifimgr
-from time import sleep
-import machine
-import ure
+# Posty Project: opensocket_server_example.py
+# Copyright (c) 2019 Clayton Darwin claytondarwin@gmail.com
 
-try:
-  import usocket as socket
-except:
-  import socket
+# notify
+print('RUN: opensocket_server_example.py')
 
-led = machine.Pin(2, machine.Pin.OUT)
+# ----------------------------------------------
+# imports
+# ----------------------------------------------
 
-wlan = wifimgr.get_connection()
-if wlan is None:
-    print("Could not initialize the network connection.")
-    while True:
-        pass  # you shall not pass :D
+# standard library imports
+import sys,time,gc,socket,machine
 
-# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
-print("ESP OK")
+# ----------------------------------------------
+# example: run opensocket_server.py
+# ----------------------------------------------
 
+# network
+from nettools import wlan_connect,wlan_disconnect
 
-# This function contains of HTML files
-def web_page(client):
-  html = """\
-        <html>
-            <h1 style="color: #5e9ca0; text-align: center;">
-                <span style="color: #ff0000;">
-                    Nurse Watch
-                </span>    
-            </h1>
-            <form action="entered" method="post">
-                <table style="margin-left: auto; margin-right: auto;">
-                    <tbody>
-            <tr>
-                            <td>Password:</td>
-                            <td><input name="Password" type="text" /></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <p style="text-align: center;">
-                    <input type="submit" value="Submit" />
-                </p>
-            </form>
-            
-    """ 
-  send_response(client,html)
-  
-# This function sends the response to the server  
-def send_response(client, payload, status_code=200):
- client.sendall("HTTP/1.0 {} OK\r\n".format(status_code))
- client.sendall("Content-Type: text/html\r\n")
- client.sendall("Content-Length: {}\r\n".format(len(payload)))
- client.sendall("\r\n")
- if len(payload) > 0:
-  client.sendall(payload)
-  
-# This function stores the value  
-def handle_save_reco(client, request):
-  match = ure.search("Password=([^&]*)",request)
-  print(match.group(0).decode("utf-8"))
-  
-# This function sends invalid request to the server
-def handle_not_found(client, url):
-    send_response(client, "Path not found: {}".format(url), status_code=404)  
+# server
+import opensocket_server
+def connectWiFi():
+   # network connect
+    essid = 'Hello1234'
+    essid_password = '12345678'
+    from nettools import wlan_connect,wlan_disconnect # must be local
+    wlan_connect(essid,essid_password,timeout=15)
     
-# Establish the bridge    
-try:
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  s.bind(('', 80))
-  s.listen(5)
-except OSError as e:
-  machine.reset()
-  
-while True:
-  try:
-    client, addr = s.accept()
-    client.settimeout(5.0)
+# run function
+def run():
+
+    # end
+    print('Nurse Watch Starting')
+    
+    #connect the Esp32 to WiFi
+    connectWiFi()
    
-    request = b""
+    # server setup
+    server = opensocket_server.OpenSocket_Server()
+    server.server_host = '0.0.0.0'
+    server.server_port = 8888
+    server.client_timeout = 10
+
+    # server start
     try:
-      while "\r\n\r\n" not in request:
-            request += client.recv(512)
-    except OSError:
-      pass
-   
-    if "HTTP" not in request:  # skip invalid requests
-                continue
-                
-    #Check URL inside request            
-    try:
-      url = ure.search("(?:GET|POST) /(.*?)(?:\\?.*?)? HTTP", request).group(1).decode("utf-8").rstrip("/")
-    except :
-      url = ure.search("(?:GET|POST) /(.*?)(?:\\?.*?)? HTTP", request).group(1).rstrip("/")
-    
-    # Categorize the request
-    if url == "":
-      web_page(client)
-    elif url == "entered":
-      handle_save_reco(client,request)
-      web_page(client)
-    else:
-      handle_not_found(client,url)
-    
-    client.close()
-  except OSError as e:
-    client.close()
-    print('Connection closed')
+        server.serve()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        sys.print_exception(e)
+
+    # network disconnect
+    wlan_disconnect()
+
+    # end
+    print('OpenSocket Server DEMO Ended')
+
+if __name__ in ('__main__','opensocket_server_example'):
+    run()
+
+# ----------------------------------------------
+# end
+# ----------------------------------------------
+
+
+
 
 
